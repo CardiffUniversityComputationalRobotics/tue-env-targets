1274bbb68fc5dadaf81334a9614eb3fe866dcac0
frkndmn <ddumanfurkan@gmail.com>
2023-08-04 18:55:05 +0100

    new branch for goal regions

--- ros-hybridplanner_simulation_bringup/install.bash
+++ ros-hybridplanner_simulation_bringup/install.bash
@@ -26,3 +26,11 @@ then
 	pip3 install .
 fi
 
+if [ ! -d ~/gazebo_models/ ]
+then
+	cucr-install-debug "Downloading gazebo models"
+	sudo apt-get install unzip
+	pip install gdown
+	gdown 1YxY81B5so6pLZ0QJK7R7bfAHoaHpPLGD -O ~/  &&  unzip ~/gazebo_models.zip -d ~/
+	rm ~/gazebo_models.zip
+fi
