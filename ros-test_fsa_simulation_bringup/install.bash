6064f61f70bb1a4d1d263e232f3beb30adb1c2fa
Ioannis Marios Stavropoulos <jstav02@gmail.com>
2024-03-08 13:55:46 +0000

    same command basically

--- ros-test_fsa_simulation_bringup/install.bash
+++ ros-test_fsa_simulation_bringup/install.bash
@@ -28,6 +28,5 @@ fi
 
 # add 'out' and 'cabinet_with_apriltag' to ~/.gazebo/models
 
-tue-install-info "Adding AprilTag models to /home/$USER/.gazebo/models/"
 mkdir -p ~/.gazebo/models
-cp ~/ros/noetic/system/src/test_fsa_simulation_bringup/worlds/models/* ~/.gazebo/models/ -r
+cp -r ~/ros/noetic/system/src/test_fsa_simulation_bringup/worlds/models/* ~/.gazebo/models/
