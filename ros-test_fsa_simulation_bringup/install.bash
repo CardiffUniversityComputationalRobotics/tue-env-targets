be4f05f208fc7b5b815fa67ecb2b8d6a64865573
Ioannis Marios Stavropoulos <jstav02@gmail.com>
2024-06-22 16:36:18 +0100

    changed models path in installation script

--- ros-test_fsa_simulation_bringup/install.bash
+++ ros-test_fsa_simulation_bringup/install.bash
@@ -30,4 +30,4 @@ fi
 
 tue-install-info "Adding AprilTag models to /home/$USER/.gazebo/models/"
 mkdir -p ~/.gazebo/models
-cp ~/ros/noetic/system/src/test_fsa_simulation_bringup/worlds/models/* ~/.gazebo/models/ -r
+cp ~/ros/noetic/repos/github.com/CardiffUniversityComputationalRobotics/shared_autonomy_manipulator/test_fsa_simulation_bringup/worlds/models/* ~/.gazebo/models/ -r
