bb89b25bbbb23f98fe8ac9ec08f983df6948f7a4
ioannismarios <jstav02@gmail.com>
2024-10-23 18:15:21 +0100

    .bashrc setup for gazebo classic

--- ros-fsa2_classic_simulation/install.bash
+++ ros-fsa2_classic_simulation/install.bash
@@ -4,4 +4,13 @@ tue-install-info "Adding AprilTag models to /home/$USER/.gazebo/models/"
 mkdir -p ~/.gazebo/models
 cp ~/ros/humble/repos/github.com/CardiffUniversityComputationalRobotics/fsa2_classic/fsa2_classic_simulation/worlds/models/* ~/.gazebo/models/ -r
 
+# setup needed for gazebo classic
+if ! grep -q "/usr/share/gazebo/setup.sh" ~/.bashrc;
+    then
+        echo "
+# setup needed for gazebo classic
+export LIBGL_ALWAYS_SOFTWARE=1
+source /usr/share/gazebo/setup.sh" >> ~/.bashrc
+    fi
+
 # echo export GZ_SIM_RESOURCE_PATH=${GZ_SIM_RESOURCE_PATH}:~/ros/humble/system/src/:~/.ignition/models/ >> ~/.bashrc
