4fddba5471f27e8873c601d9f9eb68f02f5eddf4
samuel <samuelmillannorman123@gmail.com>
2025-01-31 14:38:57 +0000

    [Add]: Jan 2025 update

---
+++ ros-ros_gzharmonic/install.bash
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+sudo curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+sudo apt-get update
+echo "Installing GZ Sim Harmonic"
+sudo apt-get install gz-harmonic
+echo "Finished installing GZ Sim Harmonic"
