b50167cf4834a8a1529599b629655e6fc38979d2
ioannismarios <jstav02@gmail.com>
2025-02-25 14:17:00 +0000

    libfranka 0.15.0 instead of 0.13.3, fmt system source, libpoco-dev name error target

--- ros-libfranka/install.bash
+++ ros-libfranka/install.bash
@@ -5,20 +5,27 @@ if [ ! -d ~/libfranka/ ]; then
     # cd ~/ros/humble/repos/github.com/frankaemika/
     cd ~
     sudo apt remove "*libfranka*"
-	sudo apt install build-essential cmake git libpoco-dev libeigen3-dev python3-rosdep2
-    git clone --recursive https://github.com/frankaemika/libfranka.git --branch 0.13.3
+    sudo apt install build-essential cmake git libpoco-dev libeigen3-dev python3-rosdep2 libfmt-dev
+    sudo apt-get install -y lsb-release curl
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL http://robotpkg.openrobots.org/packages/debian/robotpkg.asc | sudo tee /etc/apt/keyrings/robotpkg.asc
+    echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/robotpkg.asc] http://robotpkg.openrobots.org/packages/debian/pub $(lsb_release -cs) robotpkg" | sudo tee /etc/apt/sources.list.d/robotpkg.list
+    sudo apt-get update
+    sudo apt-get install -y robotpkg-pinocchio
+    # git clone --recursive https://github.com/frankaemika/libfranka.git --branch 0.13.3
+    git clone --recursive https://github.com/frankaemika/libfranka.git --branch 0.15.0
+    git submodule update
     cd libfranka
     mkdir build
     cd build
-    cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF  ..
+    cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF ..
     cmake --build . -j$(nproc)
     cpack -G DEB
     sudo dpkg -i libfranka-*.deb
     cd ~/ros/humble/system/
     rosdep update
-	rosdep install --from-paths src --ignore-src --rosdistro humble -y --skip-keys libfranka
+    rosdep install --from-paths src --ignore-src --rosdistro humble -y --skip-keys libfranka
 fi
-
 
 # if [ ! -d ~/libfranka/ ]
 # then
