cea15a9de8623847ffee5afabc25e0951de770ae
ioannismarios <jstav02@gmail.com>
2025-04-03 13:38:19 +0100

    check to not re-add gz sim resource path in bashrc

--- ros-fsa2_simulation/install.bash
+++ ros-fsa2_simulation/install.bash
@@ -1,7 +1,13 @@
 # add 'out' and 'cabinet_with_apriltag' to ~/.gazebo/models
 
-tue-install-info "Adding AprilTag models to /home/$USER/.gazebo/models/"
+# tue-install-info "Adding AprilTag models to /home/$USER/.gazebo/models/"
 mkdir -p ~/.gazebo/models
 cp ~/ros/humble/repos/github.com/CardiffUniversityComputationalRobotics/fsa2/fsa2_simulation/worlds/models/* ~/.gazebo/models/ -r
 
 echo export GZ_SIM_RESOURCE_PATH=${GZ_SIM_RESOURCE_PATH}:~/ros/humble/system/src/:~/.gazebo/models/ >> ~/.bashrc
+
+# setup needed for gazebo classic
+if ! grep -q "export GZ_SIM_RESOURCE_PATH=" ~/.bashrc;
+    then
+        echo export GZ_SIM_RESOURCE_PATH=${GZ_SIM_RESOURCE_PATH}:~/ros/humble/system/src/:~/.gazebo/models/ >> ~/.bashrc
+fi
