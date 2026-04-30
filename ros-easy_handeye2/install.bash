f01a7ad5e88a976db3a0c3b71211148c6fb8cb42
ioannismarios <jstav02@gmail.com>
2026-04-30 20:05:04 +0100

    fsa2_common -> fsa2_core

--- ros-easy_handeye2/install.bash
+++ ros-easy_handeye2/install.bash
@@ -2,4 +2,4 @@
 
 # tue-install-info "Adding AprilTag models to /home/$USER/.gazebo/models/"
 mkdir -p ~/.ros2/easy_handeye2/calibrations
-cp ~/ros/humble/repos/github.com/CardiffUniversityComputationalRobotics/fsa2/fsa2_common/cfg/* ~/.ros2/easy_handeye2/calibrations/ -r
+cp ~/ros/humble/repos/github.com/ioannismarios/fsa2_vc/fsa2_core/cfg/* ~/.ros2/easy_handeye2/calibrations/ -r
